ff4917bb34643a40bbc40fe80db7cda03db0c440
Mateusz Molka <b.0910.molmat@gmail.com>
2017-02-04 10:45:53 +0100

    4-arrays

--- JavaScript/js/4-array-vs-variable.js
+++ JavaScript/js/4-array-vs-variable.js
@@ -1,2 +1,15 @@
 'use strict';
 
+var imiona= new Array();
+
+var imionaStudentow= new Array(9);
+var imionaChlopakow= new Array('Mateusz','Krzysiek','Michal');
+imionaStudentow[1]='Marian';
+imionaStudentow[4]='Grzes';
+
+var wiek=[];
+var wiekStudentow=[20,25,28];
+
+
+console.log(imionaChlopakow);
+console.log(imionaStudentow);
