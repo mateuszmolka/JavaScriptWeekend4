3c5b63b3b0bb2e8dc9ef7dc0f178195dc326fefe
Mateusz Molka <b.0910.molmat@gmail.com>
2017-02-04 12:43:44 +0100

    8-loops

---
+++ JavaScript/js/8-loops.js
@@ -0,0 +1,53 @@
+'use strict';
+/*
+
+pętla for
+
+*/
+
+for (var i=10; i>0; i--){
+//    console.log(i);
+}
+//postinkrementacja
+for (var i=1; i<10; i++){
+//    console.log(++i);
+}
+
+/*
+
+pętla while
+
+*/
+
+var d=5;
+while(d){
+//    console.log(d);
+    d -= 1;
+}
+
+/*
+pętla do while
+    */
+
+var iter=20;
+do {
+   /* console.log(iter);
+    iter++;*/
+}while (iter <10);
+/*console.log(iter);*/
+
+//While and break
+
+var number=20;
+
+while(number>0){
+//    console.log(number);
+    number-=1;
+    if (number==10){
+        number-=5;
+    }
+}
+
+
+
+
