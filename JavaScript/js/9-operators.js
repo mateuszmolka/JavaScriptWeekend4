1ef7a499643efd2d65ddbbce902f0fd53aee4707
Mateusz Molka <b.0910.molmat@gmail.com>
2017-02-04 13:02:18 +0100

    9-operators

---
+++ JavaScript/js/9-operators.js
@@ -0,0 +1,20 @@
+'use strict';
+var liczba1=5, liczba2=2;
+
+var wynik=liczba1 % liczba2;
+console.log(wynik);
+
+liczba1 -= 3;
+
+console.log(liczba1);
+
+(2>5) ? console.log("tak") : console.log("nie");
+
+console.log("----- ACHTUNG MINEN ------------");
+
+
+
+
+
+
+
